452f54899cd20c8380516eed5a42dcb8f2c142a4
Jvymvn <713Ellington@gmail.com>
2023-12-20 19:27:19 -0500

    added tech icons to cards

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Card, Button, Image } from 'react-bootstrap';
-import placeholderimage from "./images/placeholder.png"
-import timerImage from "./images/timer-image.png"
+import { placeholderimage, timerImage, htmlSvg, cssSvg, jsSvg } from './components/Images';
+
+const techs = {
+  html: htmlSvg,
+  css: cssSvg,
+  jscript: jsSvg,
+};
 
 const data = [
-  { title: 'Countdown Timer', body: 'Count down the days, hours, minutes, and seconds until an event!', siteUrl:"eclectic-liger-410959.netlify.app", img: timerImage },
-  { title: 'Card 2', body: 'This is the body of card 2', siteUrl:"www.google.com", img: placeholderimage },
-  { title: 'Card 3', body: 'This is the body of card 3', siteUrl:"www.google.com", img: placeholderimage },
-  { title: 'Card 4', body: 'This is the body of card 4', siteUrl:"www.google.com", img: placeholderimage },
-  { title: 'Card 5', body: 'This is the body of card 5', siteUrl:"www.google.com", img: placeholderimage },
-  { title: 'Card 6', body: 'This is the body of card 6', siteUrl:"www.google.com", img: placeholderimage },
-  { title: 'Card 7', body: 'This is the body of card 7', siteUrl:"www.google.com", img: placeholderimage },
-  { title: 'Card 8', body: 'This is the body of card 8', siteUrl:"www.google.com", img: placeholderimage },
-  { title: 'Card 9', body: 'This is the body of card 9', siteUrl:"www.google.com", img: placeholderimage },
+  { id: 1, title: 'Countdown Timer', body: 'Count down the days, hours, minutes, and seconds until an event!', siteUrl: 'eclectic-liger-410959.netlify.app', img: timerImage, icons: ['html', 'css', 'jscript'] },
+  { id: 2, title: 'Card 2', body: 'This is the body of card 2', siteUrl: 'www.google.com', img: placeholderimage, icons: ['html', 'css'] },
+  { title: 'Card 3', body: 'This is the body of card 3', siteUrl: "www.google.com", img: placeholderimage, icons: [] },
+  { title: 'Card 4', body: 'This is the body of card 4', siteUrl: "www.google.com", img: placeholderimage, icons: [] },
+  { title: 'Card 5', body: 'This is the body of card 5', siteUrl: "www.google.com", img: placeholderimage, icons: [] },
+  { title: 'Card 6', body: 'This is the body of card 6', siteUrl: "www.google.com", img: placeholderimage, icons: [] },
+  { title: 'Card 7', body: 'This is the body of card 7', siteUrl: "www.google.com", img: placeholderimage, icons: [] },
+  { title: 'Card 8', body: 'This is the body of card 8', siteUrl: "www.google.com", img: placeholderimage, icons: [] },
+  { title: 'Card 9', body: 'This is the body of card 9', siteUrl: "www.google.com", img: placeholderimage, icons: [] },
 ];
 
-const cards = data.map((card, index) => (
-  <Col key={index} xs={12} md={4}>
-    <Card style={{ width: '25rem', margin: "15px" }}>
+const TechIcons = ({ technologies }) => (
+  <div className="tech-icons">
+    {/* Add your SVG icons here based on the technologies array */}
+    {technologies.map((tech, index) => (
+      <img key={index} src={techs[tech]} alt={tech} className="tech-icon" style={{ maxWidth: '25px', maxHeight: '25px' }} />
+    ))}
+  </div>
+);
+
+const cards = data.map((card) => (
+  <Col key={card.id} xs={12} md={4}>
+    <Card style={{ width: '25rem', margin: '15px' }}>
       <Image className='' src={card.img} thumbnail />
       <Card.Body>
+        <TechIcons technologies={card.icons} />
+        <br/>
         <Card.Title>{card.title}</Card.Title>
-        <Card.Text>
-          {card.body}
-        </Card.Text>
-        <Button href={`https://${card.siteUrl}`} target="_blank">Visit App</Button>
+        <Card.Text>{card.body}</Card.Text>
+        <Button href={`https://${card.siteUrl}`} target="_blank">
+          Visit App
+        </Button>
+        
       </Card.Body>
     </Card>
   </Col>
---
+++ src/components/Images.js
@@ -0,0 +1,13 @@
+import placeholderimage from "../images/placeholder.png"
+import timerImage from "../images/timer.png"
+import htmlSvg from "../images/html.svg"
+import cssSvg from "../images/css.svg"
+import jsSvg from "../images/jscript.svg"
+
+export {
+    placeholderimage,
+    timerImage,
+    htmlSvg,
+    cssSvg,
+    jsSvg,
+};
